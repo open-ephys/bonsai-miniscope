855c1bc3b8b395d5eff22f298059c324d88df5f7
jonnew <jpn@open-ephys.org>
2025-07-21 18:47:26 -0400

    Fix QuaternionToEuler transform
    - Now converts quaternion to tait bryan representation using the same
      conventions as is used internally for the Bno055.
    - Add a TaitBryanAngles type to hold these values to represent these
      angles unambiguously

--- OpenEphys.Miniscope/QuaternionToEuler.cs
+++ OpenEphys.Miniscope/QuaternionToEuler.cs
@@ -6,20 +6,31 @@ using System.Reactive.Linq;
 
 namespace OpenEphys.Miniscope
 {
-    [Description("Transforms a quaternion into axis angle representation. " +
-        "Yaw: rotation about Y axis, Pitch: rotation about Z axis, Roll: rotation about X axis.")]
-    public class QuaternionToEuler : Transform<Quaternion, Vector3>
+    [Description("Transforms a quaternion into axis angle representation using the Tait-Bryan formalism. " +
+        "Starting with ENU coordinates, Yaw represents the bearing, which is the rotation about Z/Up. " +
+        "Pitch represents the elevation, which is the rotation about Y' after application of Yaw to the ENU coordinates. " +
+        "Roll gives the bank angle, which is the rotation about X'' after application of Yaw and Pitch to the " +
+        "the ENU coordinates.")]
+    public class QuaternionToEuler : Transform<Quaternion, TaitBryanAngles>
     {
-        public override IObservable<Vector3> Process(IObservable<Quaternion> source)
+        public override IObservable<TaitBryanAngles> Process(IObservable<Quaternion> source)
         {
             // These axis definitions match those for the Euler angles in registers 0x1A through 0x1F
             return source.Select(q =>
             {
-                var yaw = MathF.Atan2(2.0f * (q.Y * q.W + q.Z * q.X), 1.0f - 2.0f * (q.Z * q.Z + q.Y * q.Y)); // Rotation about Y axis
-                var pitch = MathF.Asin(2.0f * (q.Z * q.W - q.Y * q.X)); // Rotation about Z axis
-                var roll = MathF.Atan2(2.0f * (q.Z * q.Y + q.X * q.W), 1.0f - 2.0f * (q.Z * q.Z + q.X * q.X)); // Rotation about X axis
+                var sinrCosp = 2 * (q.W * q.X + q.Y * q.Z);
+                var cosrCosp = 1 - 2 * (q.X * q.X + q.Y * q.Y);
+                var roll = MathF.Atan2(sinrCosp, cosrCosp) * 180f / MathF.PI; ;
 
-                return new Vector3(yaw, pitch, roll);
+                var sinp = MathF.Sqrt(1 + 2 * (q.W * q.Y - q.X * q.Z));
+                var cosp = MathF.Sqrt(1 - 2 * (q.W * q.Y - q.X * q.Z));
+                var pitch = (2 * MathF.Atan2(sinp, cosp) - MathF.PI / 2) * 180f / MathF.PI; ;
+
+                var sinyCosp = 2 * (q.W * q.Z + q.X * q.Y);
+                var cosyCosp = 1 - 2 * (q.Y * q.Y + q.Z * q.Z);
+                var yaw = (MathF.Atan2(sinyCosp, cosyCosp) + MathF.PI) * 180f / MathF.PI;
+
+                return new TaitBryanAngles(yaw, pitch, roll);
             });
         }
     }
