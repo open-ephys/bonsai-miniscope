a70ba055e887d1228c1fb6bf970d07a7193680f6
Aaron Cuevas Lopez <1830303+aacuevas@users.noreply.github.com>
2026-04-24 02:58:24 +0200

    Add XmlComments and removed extra semicolons

--- OpenEphys.Miniscope/QuaternionToEuler.cs
+++ OpenEphys.Miniscope/QuaternionToEuler.cs
@@ -6,6 +6,14 @@ using System.Reactive.Linq;
 
 namespace OpenEphys.Miniscope
 {
+    /// <summary>
+    /// Represents an operator that transforms a sequence of quaternions into an axis-angle representation using the Tait-Bryan formalism.
+    /// </summary>
+    /// <remarks>
+    /// Starting with ENU coordinates, Yaw represents the bearing, which is the rotation about Z/Up. 
+    /// Pitch represents the elevation, which is the rotation about Y' after application of Yaw to the ENU coordinates. 
+    /// Roll gives the bank angle, which is the rotation about X'' after application of Yaw and Pitch to the ENU coordinates.
+    /// </remarks>
     [Description("Transforms a quaternion into axis angle representation using the Tait-Bryan formalism. " +
         "Starting with ENU coordinates, Yaw represents the bearing, which is the rotation about Z/Up. " +
         "Pitch represents the elevation, which is the rotation about Y' after application of Yaw to the ENU coordinates. " +
@@ -13,6 +21,15 @@ namespace OpenEphys.Miniscope
         "the ENU coordinates.")]
     public class QuaternionToEuler : Transform<Quaternion, TaitBryanAngles>
     {
+        /// <summary>
+        /// Transforms an observable sequence of quaternions into Tait-Bryan angles.
+        /// </summary>
+        /// <param name="source">
+        /// An observable sequence of <see cref="Quaternion"/> values representing the spatial rotations to transform.
+        /// </param>
+        /// <returns>
+        /// A sequence of <see cref="TaitBryanAngles"/> where each value represents the corresponding rotation decomposed into yaw, pitch, and roll.
+        /// </returns>
         public override IObservable<TaitBryanAngles> Process(IObservable<Quaternion> source)
         {
             // These axis definitions match those for the Euler angles in registers 0x1A through 0x1F
@@ -20,11 +37,11 @@ namespace OpenEphys.Miniscope
             {
                 var sinrCosp = 2 * (q.W * q.X + q.Y * q.Z);
                 var cosrCosp = 1 - 2 * (q.X * q.X + q.Y * q.Y);
-                var roll = MathF.Atan2(sinrCosp, cosrCosp) * 180f / MathF.PI; ;
+                var roll = MathF.Atan2(sinrCosp, cosrCosp) * 180f / MathF.PI;
 
                 var sinp = MathF.Sqrt(1 + 2 * (q.W * q.Y - q.X * q.Z));
                 var cosp = MathF.Sqrt(1 - 2 * (q.W * q.Y - q.X * q.Z));
-                var pitch = (2 * MathF.Atan2(sinp, cosp) - MathF.PI / 2) * 180f / MathF.PI; ;
+                var pitch = (2 * MathF.Atan2(sinp, cosp) - MathF.PI / 2) * 180f / MathF.PI;
 
                 var sinyCosp = 2 * (q.W * q.Z + q.X * q.Y);
                 var cosyCosp = 1 - 2 * (q.Y * q.Y + q.Z * q.Z);
